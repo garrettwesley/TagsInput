c1a7b6ca83dfe0ddc1f7fe9b0f19124ff79bdf4d
Garrett Wesley <garrettwesley@berkeley.edu>
2021-01-06 12:03:35 -0800

    Best so far

--- TagsInput/ContentView.swift
+++ TagsInput/ContentView.swift
@@ -1,22 +1,24 @@
-//
-//  ContentView.swift
-//  TagsInput
-//
-//  Created by Garrett Wesley on 1/5/21.
-//
-
-import SwiftUI
-
-struct TagsInput: View {
+ //
+ //  TagsInput.swift
+ //  TagsInput
+ //
+ //  Created by Garrett Wesley on 1/5/21.
+ //
+ 
+ import SwiftUI
+ 
+ struct TagsInput: View {
     @State var tags: [String] = ["test"]
     @State var test = ""
     
     var body: some View {
-        VStack {
-            TagsController(tags: $tags, addTag: addTag, removeTag: removeTag)
-        }
+        TagsController(tags: $tags, addTag: addTag, removeTag: removeTag, shouldAddTag: shouldAddTag)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.blue, lineWidth: 2)
+            )
     }
-        
+    
     func addTag(_ val: String) {
         tags.append(val)
     }
@@ -24,9 +26,33 @@ struct TagsInput: View {
     func removeTag(_ i: Int) {
         tags.remove(at: i)
     }
-}
-
-public struct TagsController: View {
+    
+    func shouldAddTag(_ tag: String, _ tags: [String]) -> Bool {
+        return !(tags.contains(tag))
+    }
+ }
+ 
+ struct TagsOptions {
+    struct TagItemOptions {
+        let insets: EdgeInsets = .init(top: 6, leading: 12, bottom: 6, trailing: 12)
+        let cornerRadius: CGFloat = 8
+        let font: Font = .system(size: 14)
+        
+        let textColor: Color = .white
+        let selectedTextColor: Color = .white
+        let backgroundColor: Color = .gray
+        let selectedColor: Color = .red
+    }
+    
+    let tagItemOptions: TagItemOptions = TagItemOptions()
+    let tintColor: Color = Color(UIColor.blue)
+    let newTagCharacter: String = " "
+    let spaceBetweenTags: CGFloat = 4
+    let spaceBetweenLines: CGFloat = 4
+    let placeholder: String = "Tags..."
+ }
+ 
+ public struct TagsController: View {
     struct SizePreferenceKey: PreferenceKey {
         typealias Value = [CGSize]
         static var defaultValue: Value = []
@@ -36,8 +62,10 @@ public struct TagsController: View {
     }
     
     @Binding var tags: [String]
-    var addTag: (String) -> ()
-    var removeTag: (Int) -> ()
+    let addTag: (String) -> ()
+    let removeTag: (Int) -> ()
+    let shouldAddTag: (String, [String]) -> (Bool)
+    let options: TagsOptions = TagsOptions()
     
     @State private var sizes: [CGSize] = []
     @State private var text: String = ""
@@ -49,9 +77,9 @@ public struct TagsController: View {
         selection = nil
         if val.count >= last.count {
             if val.last == " " {
-                let str = String(val[..<val.lastIndex(of: " ")!])
-                if !(tags.contains(str)) && str.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 {
-                    addTag(str)
+                let tag = String(val[..<val.lastIndex(of: " ")!])
+                if shouldAddTag(tag, tags) && tag.trimmingCharacters(in: .whitespacesAndNewlines).count > 0 {
+                    addTag(tag)
                 }
                 text = ""
             }
@@ -64,31 +92,31 @@ public struct TagsController: View {
         if selection != nil {
             removeTag(selection!)
             selection = nil
-        } else if tags.count > 0 {
+        } else if tags.count > 0 && text.count == 0 {
             selection = tags.count - 1
         }
     }
     
     public var body: some View {
-        GeometryReader {geometry in
+        GeometryReader { geometry in
             ZStack(alignment: .topLeading) {
                 ForEach(tags.indices, id: \.self) { i in
                     Group {
-                        TagItem(active: $selection, index: i, text: tags[i])
+                        TagItem(active: $selection, index: i, text: tags[i], options: options.tagItemOptions)
                     }
                     .onTapGesture {
                         selection = i
                     }
-                    .padding(.horizontal, 4)
-                    .padding(.bottom, 4)
+                    .padding(.trailing, options.spaceBetweenTags)
+                    .padding(.bottom, options.spaceBetweenLines)
                     .background(backgroundView())
                     .offset(getOffset(at: i, geometry: geometry))
                 }
                 TagsTextField(text: $text, didDelete: didDelete)
                     .frame(width: getWidth(from: geometry), height: 30)
                     .onChange(of: text, perform: handleInput)
-                    .padding(.leading, 8)
-                    .padding(.bottom, 4)
+                    .padding(.horizontal, options.spaceBetweenLines)
+                    .padding(.bottom, options.spaceBetweenLines)
                     .background(backgroundView())
                     .offset(getInputOffset(geometry))
                     .accentColor(selection == nil ? Color.blue : Color.clear)
@@ -103,7 +131,7 @@ public struct TagsController: View {
     
     private func getWidth(from g: GeometryProxy) -> CGFloat {
         guard tags.count > 0 && sizes.count > 0 else { return g.size.width }
-
+        
         let w = g.size.width
         let (lastX, _, _) = sizes[..<(sizes.count - 1)].reduce((CGFloat.zero,CGFloat.zero,CGFloat.zero)) {
             var (x,y,maxHeight) = $0
@@ -117,12 +145,13 @@ public struct TagsController: View {
             return (x,y,maxHeight)
         }
         
-        let spaceLeft = w - lastX - 15
+        let spaceLeft = w - lastX - (options.spaceBetweenTags * 2)
         return spaceLeft > minWidth ? spaceLeft : w
     }
     
     private func getInputOffset(_ g: GeometryProxy) -> CGSize {
         guard sizes.count > 0 else { return .zero }
+        print(sizes.count)
         return getOffset(at: sizes.endIndex - 1, geometry: g)
     }
     
@@ -157,28 +186,36 @@ public struct TagsController: View {
                 )
         }
     }
-}
-
-public struct TagItem: View {
+ }
+ 
+ public struct TagItem: View {
     @State private var color: Color = Color.gray
     @Binding var active: Int?
+    var opts: TagsOptions.TagItemOptions
     var index: Int
     var text: String
     
+    init(active: Binding<Int?>, index: Int, text: String, options: TagsOptions.TagItemOptions) {
+        self._active = active
+        self.text = text
+        self.index = index
+        self.opts = options
+        self.color = options.backgroundColor
+    }
+    
     public var body: some View {
         Text(text)
-            .padding(.vertical, 6)
-            .padding(.horizontal, 12)
-            .font(.system(size: 14))
+            .padding(opts.insets)
+            .font(opts.font)
             .background(color)
-            .foregroundColor(Color.white)
-            .cornerRadius(8)
+            .foregroundColor((active == index) ? opts.selectedTextColor : opts.textColor)
+            .cornerRadius(opts.cornerRadius)
             .lineLimit(1)
             .truncationMode(.tail)
             .onChange(of: active, perform: { val in
                 withAnimation(.easeInOut(duration: 0.2)) {
-                    self.color = val == index ? .red : .gray
+                    self.color = val == index ? opts.selectedColor : opts.backgroundColor
                 }
             })
     }
-}
+ }
