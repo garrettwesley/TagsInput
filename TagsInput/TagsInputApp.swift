2896847328170630f9945dddbbe137382a3b3e93
Garrett Wesley <garrettwesley@berkeley.edu>
2021-01-05 17:45:04 -0800

    Initial Commit

---
+++ TagsInput/TagsInputApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagsInputApp.swift
+//  TagsInput
+//
+//  Created by Garrett Wesley on 1/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TagsInputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
