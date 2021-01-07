093b9a97aefbba7c62c444df3537134dbbef0247
Garrett Wesley <garrettwesley@berkeley.edu>
2021-01-07 14:06:33 -0800

    ...

--- TagsInput/FinalInputs.swift
+++ TagsInput/FinalInputs.swift
@@ -1,58 +1,54 @@
 //
-//  TagsInput.swift
-//  TagsInput
+//  FinalInput.swift
+//  happen
 //
-//  Created by Garrett Wesley on 1/5/21.
+//  Created by Garrett Wesley on 1/7/21.
+//  Copyright © 2020 Garrett Wesley. All rights reserved.
 //
- 
+
 import SwiftUI
 import Foundation
- 
-struct TagsInput: View {
-    @State var tags: [String] = ["first"]
-    
-    var body: some View {
-        OldTagsInput(tags: $tags)
-            .padding(20)
-            .background(RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 2)
-            )
+
+struct SizePref: PreferenceKey {
+    static var defaultValue: CGSize = .init(width: 0, height: 0)
+    static func reduce(value: inout CGSize , nextValue: () -> CGSize) {
+        value = nextValue()
     }
+}
 
- }
-
- struct TagsOptions {
+struct TagsOptions {
     struct TagItemOptions {
         let insets: EdgeInsets = .init(top: 6, leading: 14, bottom: 6, trailing: 14)
         let cornerRadius: CGFloat = 8
         let fontSize: CGFloat = 14
         
         let textColor: Color = .gray
-        let selectedTextColor: Color = .gray
+        let selectedTextColor: Color = .white
         let backgroundColor: Color = .whiter
-        let selectedColor: Color = .red
+        let selectedColor: Color = .accentColor
     }
     
     let tagItemOptions: TagItemOptions = TagItemOptions()
     let tagsInsets: EdgeInsets = .init(top: 6, leading: 12, bottom: 6, trailing: 12)
-    let tintColor: Color = Color(UIColor.blue)
+    let tintColor: Color = .accentColor
     let newTagCharacter: String = " "
     let spaceBetweenTags: CGFloat = 8
     let spaceBetweenLines: CGFloat = 6
     let placeholder: String = "Add a tag"
- }
+}
 
 
-public struct OldTagsInput: View {
+public struct TagsInput: View {
     @Binding var tags: [String]
+    @Binding var height: CGFloat
+    
     let options: TagsOptions = TagsOptions()
     
-    @State private var height: CGFloat = .zero
     @State private var text: String = ""
     @State private var last: String = ""
     @State private var selection: Int? = nil
     private let minWidth: CGFloat = 60
-
+    
     func handleInput(_ val: String) {
         selection = nil
         if val.count >= last.count {
@@ -87,10 +83,8 @@ public struct OldTagsInput: View {
             self.generateContent(with: g)
                 .frame(height: height)
                 .onPreferenceChange(SizePref.self, perform: { newSize in
-                    self.height = newSize.height
-                    print("New height was updated to:", self.height)
+                    $height.wrappedValue = newSize.height
                 })
-                .background(Color.red)
         }
     }
     
@@ -129,19 +123,17 @@ public struct OldTagsInput: View {
                             return currY
                         })
                 }
-    //            TagsTextField(text: $text, fontSize: options.tagItemOptions.fontSize, didDelete: didDelete)
-                TextField(tags.isEmpty ? options.placeholder : "", text: $text)
-                    .frame(width: calcInputWidth(g.size.width), height: options.tagItemOptions.fontSize + options.tagItemOptions.insets.top + options.tagItemOptions.insets.bottom, alignment: .bottom)
+                TagsTextField(text: $text, fontSize: options.tagItemOptions.fontSize, placeholder: tags.isEmpty ? options.placeholder : "", didDelete: didDelete)
+//                TextField(tags.isEmpty ? options.placeholder : "", text: $text)
+                    .frame(width: calcInputWidth(g.size.width), height: calcInputHeight())
                     .onChange(of: text, perform: handleInput)
-                    .padding(.leading, options.spaceBetweenTags)
+                    .padding(.trailing, options.spaceBetweenTags)
                     .padding(.bottom, options.spaceBetweenLines)
-                    .background(Color.gray)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
-                    .accentColor(selection == nil ? Color.blue : Color.clear)
+                    .accentColor(selection == nil ? options.tintColor : Color.clear)
                     .onTapGesture { selection = nil }
                     .alignmentGuide(.leading, computeValue: { t in
-//                        print(maxWidth, "curr size:", t.width)
                         if (abs(currX - t.width) > maxWidth) {
                             currX = 0
                             currY -= t.height
@@ -159,11 +151,13 @@ public struct OldTagsInput: View {
             key: SizePref.self,
             value: .bounds,
             transform: { rect in
-                print("Size changed to:", g[rect].size)
                 return g[rect].size
             }
         )
-        .background(Color.yellow)
+    }
+    
+    private func calcInputHeight() -> CGFloat {
+        return "TEST".size(withAttributes:[.font: UIFont.systemFont(ofSize: options.tagItemOptions.fontSize)]).height + options.tagItemOptions.insets.top + options.tagItemOptions.insets.bottom
     }
     
     private func calcInputWidth(_ maxWidth: CGFloat) -> CGFloat {
@@ -179,14 +173,12 @@ public struct OldTagsInput: View {
         }
         
         let spaceLeft = maxWidth - lastX - (2 * options.spaceBetweenTags)
-        let res = spaceLeft > minWidth ? spaceLeft : maxWidth - (2*options.spaceBetweenTags)
-//        print("keyboard width: ", res)
-        return res
+        return spaceLeft > minWidth ? spaceLeft : maxWidth - (2*options.spaceBetweenTags)
     }
 }
 
 public struct TagItem: View {
-    @State private var color: Color = Color.gray
+    @State private var color: Color = Color.whiter
     @Binding var active: Int?
     var opts: TagsOptions.TagItemOptions
     var index: Int
@@ -199,36 +191,88 @@ public struct TagItem: View {
         self.index = index
         self.opts = options
         self.handleRemove = handleRemove
-
+        
         self.color = options.backgroundColor
     }
     
     public var body: some View {
-        ZStack(alignment: .center) {
-            Text(text)
-                .font(.system(size: opts.fontSize))
-                .foregroundColor((active == index) ? opts.selectedTextColor : opts.textColor)
-                .lineLimit(1)
-                .truncationMode(.tail)
-                .opacity((active == index) ? 0 : 1)
-            if active == index {
-                Button(action: { handleRemove(index) }) {
-                    Image(systemName: "x.circle")
-                        .font(.system(size: opts.fontSize))
-                        .foregroundColor(opts.selectedTextColor)
+        Text(text)
+            .font(.system(size: opts.fontSize))
+            .foregroundColor((active == index) ? opts.selectedTextColor : opts.textColor)
+            .lineLimit(1)
+            .truncationMode(.tail)
+            .padding(opts.insets)
+            .background(color)
+            .cornerRadius(opts.cornerRadius)
+            .onChange(of: active, perform: { val in
+                withAnimation(.easeInOut(duration: 0.1)) {
+                    self.color = (val == index) ? opts.selectedColor : opts.backgroundColor
                 }
-                .opacity((active == index) ? 1 : 0)
-            }
-            
+            })
+    }
+}
+
+
+import UIKit
+
+protocol DeleteTextFieldDelegate: AnyObject {
+    func textFieldDidDelete()
+}
+
+class DeleteTextField: UITextField {
+
+    weak var myDelegate: DeleteTextFieldDelegate?
+
+    override func deleteBackward() {
+        super.deleteBackward()
+        myDelegate?.textFieldDidDelete()
+    }
+
+}
+
+struct TagsTextField: UIViewRepresentable {
+    @Binding var text: String
+    var fontSize: CGFloat
+    var placeholder: String = ""
+    var didDelete: () -> ()
+
+    func makeUIView(context: Context) -> UITextField {
+        let textField = DeleteTextField()
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.font = .systemFont(ofSize: fontSize)
+        textField.placeholder = placeholder
+        textField.delegate = context.coordinator
+        textField.myDelegate = context.coordinator
+        return textField
+    }
+
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        uiView.text = text // 1. Read the binded
+        uiView.placeholder = placeholder
+    }
+
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(text: $text, didDelete: didDelete)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate, DeleteTextFieldDelegate {
+        @Binding var text: String
+        var didDelete: () -> ()
+
+        init(text: Binding<String>, didDelete: @escaping () -> ()) {
+            self._text = text
+            self.didDelete = didDelete
         }
-        .fixedSize()
-        .padding(opts.insets)
-        .background(color)
-        .cornerRadius(opts.cornerRadius)
-        .onChange(of: active, perform: { val in
-            withAnimation(.easeInOut(duration: 0.1)) {
-                self.color = (val == index) ? opts.selectedColor : opts.backgroundColor
+        
+        func textFieldDidDelete() {
+            didDelete()
+        }
+
+        func textFieldDidChangeSelection(_ textField: UITextField) {
+            DispatchQueue.main.async {
+                self.text = textField.text ?? ""
             }
-        })
+        }
     }
 }
